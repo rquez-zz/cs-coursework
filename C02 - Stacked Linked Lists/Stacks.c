85f6743edff9ff2cb3e8b3170e91109353fc76b8
rquez <rvzxjr@gmail.com>
2015-02-18 09:39:11 -0500

    Started Assignment 2

---
+++ C02 - Stacked Linked Lists/Stacks.c
@@ -0,0 +1,21 @@
+/*
+ * groceries.c
+ *
+ *  Created on: Feb 18, 2015
+ *      Author: Ricardo Vasquez
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main () {
+
+	// Set stream buffer for eclipse console
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+
+
+
+	return 0;
+}
