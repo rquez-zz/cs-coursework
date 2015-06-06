c5210337ee502b77258907468bd2004f393b9b31
rquez <rvzxjr@gmail.com>
2015-06-06 15:54:32 -0400

    Add clean assignment files

---
+++ D03 - Basics III/CS2RecitationWeek3.java
@@ -0,0 +1,88 @@
+import java.util.Scanner;
+
+public class CS2RecitationWeek3 
+{
+    ///////////////////////////////////////////
+	//
+	// Start of assignment code.
+	//
+	///////////////////////////////////////////
+	
+        /*
+            Note: All of the following assignments must be completed without the use of any loops including (for, while, and do-while).
+            You may not utilize them at any point in your programming to help enhance your use of recursion.
+        */
+    
+    
+    
+	/**
+	 * Returns the last name, first name, and PID of the student.
+	 * 
+	 * This is required in order to get credit for the programming assignment.
+	 */
+	static String GetNameAndPID()
+	{
+		return( "Last,First,PID");
+	}    
+        
+        /*  Directions: Recursive function that returns the value of a desired Fibonatchi number
+            Note: For all recursive functions you should always be some calls that do
+            not result in a recursive call without one your function will end up in an
+            infinite loop.
+        */
+        //FibonatchiNumber(1) -> 1
+        //FibonatchiNumber(7) -> 13
+        //FibonatchiNumber(17) -> 1597
+    
+        /*
+           @param FibonatchiNumber
+                            int of desired fibonatchi number
+           @return
+                            int value of the fibonatchi number
+        */
+        
+        static int getFibonatchiNumber(int FibonatchiNumber)
+        {
+        }
+    
+        
+        //  Directions: Recursive function that returns the value of a desired factorial
+      
+        //FactorialNumber(1) -> 1
+        //FactorialNumber(7) -> 5040
+        //FactorialNumber(12) -> 479001600
+    
+        /*
+           @param FactorialNumber
+                            int of desired Factorial number
+           @return
+                            int value of the Factoiral number
+        */
+    
+        static int getFactorialNumber(int FactorialNumber)
+        {
+        }
+    
+        /*  Directions: Recursive function that adds up the the squares of a range of two integer values
+            Example: SumSquaresOfRange(5, 10);
+            (5*5)+(6*6)+(7*7)+(8*8)+(9*9)+(10*10)=355
+        */
+      
+        
+        /*
+           @param int x , int y
+                            two integer values of the desired ranage
+           @return
+                            int value of the sum of all the numbers in the range squared
+        */
+        
+        static int SumSquaresOfRange(int x , int y)
+        {
+        }
+	
+        public static void main(String[] args)
+		{       
+        }//End of main
+
+}
+
