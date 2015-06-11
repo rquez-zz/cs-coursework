1483fa6887fe25a2f978a2b6f87fcd871e260882
rquez <rvzxjr@gmail.com>
2015-06-11 10:26:25 -0400

    Start week 4 assignment

---
+++ D04 - Backtracking/src/EightStart.java
@@ -0,0 +1,52 @@
+import java.applet.Applet;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+
+public class EightStart extends Applet implements MouseListener, Runnable {
+	// ADD your finals here
+	//8x8 board
+	// How m
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		super.init();
+		// Set size here
+		
+		// import image 
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
