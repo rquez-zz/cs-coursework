a3634fd467d812120e37b70aacc7134a4c75a17d
rquez <rvzxjr@gmail.com>
2015-06-28 18:24:08 -0400

    Improve case 0 handling

--- D06 - DualPhilosophers/src/DualPhilosophers.java
+++ D06 - DualPhilosophers/src/DualPhilosophers.java
@@ -4,10 +4,39 @@ public class DualPhilosophers {
 	
 	public static int findArragements(int[][] input)
 	{
-		if (input[0][0] == input[0][1])
+		int[] relations = relationify(input);
+
+		if (hasCrossRelation(relations))
 			return 0;
 		
-		return 0;
+		return 2;
+	}
+	
+	public static int[] relationify(int[][] input)
+	{
+		int[] relations = new int[input[0][0] + 1];
+		
+		for (int i = 1; i < input.length; i++)
+			relations[input[i][0]] = input[i][1];
+		
+		return relations;
+	}
+	
+	public static boolean hasCrossRelation(int[] relations)
+	{
+		boolean result = false;
+	
+		for (int i = 1; i < relations.length - 1; i++)
+		{
+			if (relations[i] != 0)
+			{
+				if (relations[i] == i + 1 && relations[i + 1] == i)
+				{
+					result = true;
+				}
+			}
+		}
+		return result;
 	}
 
 }
--- D06 - DualPhilosophers/src/DualPhilosophersTest.java
+++ D06 - DualPhilosophers/src/DualPhilosophersTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class DualPhilosophersTest {
 
 	@Test
-	public void testFindArragements() {
+	public void testCaseEqualRelationsAndEssays() {
 
 		int[][] input = new int [4][2];
 		input[0] = new int[]{3, 3};
@@ -17,5 +17,21 @@ public class DualPhilosophersTest {
 		assertEquals(0, DualPhilosophers.findArragements(input));
 	}
 
+	@Test
+	public void testCrossRelation() {
 
+		int[][] input = new int [4][2];
+		input[0] = new int[]{4, 3};
+		input[1] = new int[]{3, 4};
+		input[2] = new int[]{4, 3};
+		input[3] = new int[]{1, 2};
+
+		assertEquals(0, DualPhilosophers.findArragements(input));
+	}
+	
+	public void printArray(int[] array)
+	{
+		for (int i = 0; i < array.length; i++)
+			System.out.println(array[i]);
+	}
 }
