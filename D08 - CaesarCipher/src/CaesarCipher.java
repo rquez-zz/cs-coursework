1896e0fff960123a1cd30a92bae7e084f5819d4e
rquez <rvzxjr@gmail.com>
2015-07-13 19:27:28 -0400

    Start programming assignment 8

---
+++ D08 - CaesarCipher/src/CaesarCipher.java
@@ -0,0 +1,84 @@
+/**
+ * Created by Ricardo on 7/13/2015.
+ */
+public class CaesarCipher {
+
+
+    private static final double[] FREQUENCY_TABLE = {8.2, 1.5, 2.8, 4.3, 12.7, 2.2, 2.0, 6.1, 7.0, 0.2, 0.8, 4.0, 2.4,
+            6.7, 7.5, 1.9, 0.1, 6.0, 6.3, 9.1, 2.8, 1.0, 2.4, 0.2, 2.0, 0.1};
+
+    int let2nat(char c)
+    {
+
+        return 0;
+    }
+
+    char nat2let(int code)
+    {
+
+        return 'c';
+    }
+
+    char shift(int shftAmt, char c)
+    {
+
+        return c;
+    }
+
+    String encode(int shftAmt, String str)
+    {
+
+        return "";
+    }
+
+    String decode(int shftAmt, String str)
+    {
+
+        return "";
+    }
+
+    int lowers(String str)
+    {
+
+        return 0;
+    }
+
+    int count(String str)
+    {
+
+        return 0;
+    }
+
+    double percent(int num1, int num2)
+    {
+        return 0.0;
+    }
+
+    double[] freqs(String str)
+    {
+        return null;
+    }
+
+    double[] rotate(int n, double[] list)
+    {
+        return null;
+    }
+
+    double chisqr(double[] os)
+    {
+
+        return 0.0;
+    }
+
+    int position(double a, double[] list)
+    {
+        return 0;
+    }
+
+    String crack(String str)
+    {
+        return "";
+    }
+
+
+}
