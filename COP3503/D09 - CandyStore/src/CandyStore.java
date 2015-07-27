db1e995ca01120a4c993c2ed162255714dc7466b
rquez <rvzxjr@gmail.com>
2015-07-27 19:53:36 -0400

    Add main method to candystore

--- COP3503/D09 - CandyStore/src/CandyStore.java
+++ COP3503/D09 - CandyStore/src/CandyStore.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by Ricardo on 7/26/2015.
  */
@@ -15,6 +17,30 @@ public class CandyStore {
                 solve(candy + 1, calories, prices, money),
                 solve(candy, calories, prices, money - prices[candy]) + calories[candy]
             );
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        boolean isDone = false;
+        while(!isDone)
+        {
+            int numCandies = scanner.nextInt();
+            double money = scanner.nextDouble();
+            if (numCandies == 0 && money == 0.00)
+            {
+                isDone = true;
+                break;
+            }
+            int[] calories = new int[numCandies];
+            double[] prices = new double[numCandies];
+            for(int i = 0; i < numCandies; i++)
+            {
+                calories[i] = scanner.nextInt();
+                prices[i] = scanner.nextDouble();
+            }
+            System.out.println(solve(0, calories, prices, money));
+        }
     }
 }
 
