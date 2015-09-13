07e7e7cb0c1d1180815e33a5f6e9b4deecfdefff
Ricardo Vasquez <rvzxjr@gmail.com>
2015-09-12 20:12:38 -0400

    Stub out methods with comments in stack.c

---
+++ COP3402/PM0/src/stack.c
@@ -0,0 +1,18 @@
+#include <stack.h>
+
+const char* INPUT = "../input/mcode.txt";
+
+int main() {
+
+}
+
+//Returns a pointer to array of instructions read from input
+instruction* read(const char* path) {
+
+}
+
+// Executes instructions
+// Returns next index
+int execute(instruction* instruction, int index) {
+
+}
