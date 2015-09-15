890f22ddf11b3863c05d0ccdcfd91eb3aca8e252
Ricardo Vasquez <rvzxjr@gmail.com>
2015-09-15 15:59:03 -0400

    Add global constants, make stack operations a module to be called on, and stub out workflow

--- COP3402/PM0/src/stack.c
+++ COP3402/PM0/src/stack.c
@@ -1,9 +1,32 @@
-#include <stack.h>
+#include "stack.h"
 
-const char* INPUT = "../input/mcode.txt";
+const int MAX_STACK_HEIGHT = 200;
+const int MAX_CODE_LENGTH = 500;
+const int MAX_LINE_LENGTH = 10;
+const int MAX_LEXI_LEVEL = 3;
 
-int main() {
+int stack(FILE* filePtr) {
 
+    int SP = 0;
+    int BP = 0;
+    int PC = 0;
+    int IR = 0;
+
+    int halt = 0;
+
+    // Read instructions
+    instruction instructions[MAX_CODE_LENGTH];
+    read(filePtr, instructions);
+
+    int i = 0;
+    while(!halt) {
+        // Fetch next instruction
+        instruction* nextInstruction = &instructions[i - 1];
+        // Execute next instruction and get next index
+        i = execute(nextInstruction, i);
+    }
+
+    return 0;
 }
 
 //Returns a pointer to array of instructions read from input
