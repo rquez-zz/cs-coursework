f5271dff6697c051ecfb858c2449813c0704c511
Ricardo Vasquez <rvzxjr@gmail.com>
2015-09-19 16:32:13 -0400

    Pass the string path of the I/O to the stack function and let it handle I/O operations.

--- COP3402/PM0/src/pm0.c
+++ COP3402/PM0/src/pm0.c
@@ -2,33 +2,15 @@
 
 int main(int argc, char **argv) {
 
-    // Open file using first argument
+    // Get path for input file
     const char* inputPath = argv[1];
-    printf("Opening %s for input.\n", inputPath);
-    FILE* inputPtr= openFile(inputPath);
 
-    // Open output file using second argument
+    // Get path for output file
     const char* outputPath = argv[2];
-    printf("Opening %s for output.\n", outputPath);
-    FILE* outputPtr= openFile(outputPath);
 
     // Perform Stack Operations
-    stack(inputPtr, outputPtr);
-
-    // Close file
-    printf("Closing input file.\n");
-    fclose(inputPtr);
-    fclose(outputPtr);
+    printf("[LOG] Performing stack operations\n");
+    stack(inputPath, outputPath);
 
     return 0;
-}
-
-/* Opens a file and returns a FILE pointer */
-FILE* openFile(const char* path) {
-    FILE* filePtr;
-    filePtr = fopen(path, "r");
-    if(filePtr == NULL) {
-        perror("Error opening input file.");
-    }
-    return filePtr;
 }
