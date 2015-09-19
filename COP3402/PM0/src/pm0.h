fab9d05e8696df6a0f546ba8255c5a7c33c06e8e
Ricardo Vasquez <rvzxjr@gmail.com>
2015-09-19 16:23:07 -0400

    Merge together stack.h and pm0.h to make structure less complex.

--- COP3402/PM0/src/pm0.h
+++ COP3402/PM0/src/pm0.h
@@ -1,3 +1,18 @@
 #include <stdio.h>
-#include "stack.h"
-FILE* openFile(const char* path);
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    int opcode;
+    int lex;
+    int param;
+} instruction;
+
+FILE* openFile(const char* path, const char* op);
+void stack(const char* inputPath, const char* outputPath);
+void read(FILE* inputPtr, instruction* instructions);
+char* buildInstructionsString(instruction* instrutions);
+char* buildTraceLine(int prevPC,
+        instruction* IR, int PC, int BP, int SP, int* stack);
+void execute(instruction* IR, int* PC, int* SP, int* BP, int* halt, int* stack);
+char* getOpcodeName(int opcode);
