07b08bb043fe042852d5682c99010c2829477d8d
Ricardo Vasquez <rvzxjr@gmail.com>
2015-10-20 19:53:59 -0400

    Start assignment 3 on turing machines

---
+++ COT4210/F03-turing-machine/src/TM.java
@@ -0,0 +1,12 @@
+/**
+ * COT4210 Assignment 3
+ * TM - Turing Machine
+ * Author: Ricardo Vasquez
+ */
+class Rule {
+}
+public class TM {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ COT4210/F03-turing-machine/src/TMTest.java
@@ -0,0 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class TMTest {
+
+    TM tm1;
+    TM tm2;
+    TM tm3;
+    TM tm4;
+
+    @Before
+    public void setup() {
+    }
+
+    @Test
+    public void runTest() {
+    }
+}
