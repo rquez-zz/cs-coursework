3bfb974f867c9229d4e530b249d34e4089f95ab8
Ricardo Vasquez <rvzxjr@gmail.com>
2015-11-05 23:03:52 -0500

    Add parser source file

---
+++ src/parser.c
@@ -0,0 +1,10 @@
+#include "parser.h"
+
+int parse(const char* symbolTablePath, token* tokens, symbol* symbolTable) {
+
+    // Get Token
+    // Parse Token
+    // Enter Token into Symbol Table
+
+    return 0;
+}
