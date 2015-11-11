e16c2463f916249937bfa47de9ee655228952d44
Ricardo Vasquez <rvzxjr@gmail.com>
2015-11-11 14:45:25 -0500

    Stub out parsing methods

--- src/parser.c
+++ src/parser.c
@@ -1,10 +1,35 @@
 #include "parser.h"
 
-int parse(const char* symbolTablePath, token* tokens, symbol* symbolTable) {
+int parse(const char* symbolTablePath, token* tokens, symbol* symbolTablePtr) {
 
     // Get Token
     // Parse Token
     // Enter Token into Symbol Table
 
     return 0;
+}
+
+/* program := block periodsym */
+void program(token* tokens, symbol* symbolTable) {
+}
+
+/* block := [constant] [variable] {procedure} [statement] */
+void block(token* tokens, symbol* symbolTable) {
+}
+
+/* constant := constsym identsym equalsym numbersym {commasym identsym equalsym numbersym} semicolonsym*/
+void constant(token* tokens, symbol* symbolTable) {
+
+}
+
+void variable(token* tokens, symbol* symbolTable) {
+
+}
+
+void procedure(token* tokens, symbol* symbolTable) {
+
+}
+
+void statement(token* tokens, symbol* symbolTable) {
+
 }
--- src/parser.h
+++ src/parser.h
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 #include "symbol.h"
 #include "token.h"
 
 int parse(const char* symbolTablePath, token* tokens, symbol* symbolTable);
+void program(token* tokens, symbol* symbolTable);
+void block(token* tokens, symbol* symbolTable);
+void constant(token* tokens, symbol* symbolTable);
+void variable(token* tokens, symbol* symbolTable);
+void procedure(token* tokens, symbol* symbolTable);
+void statement(token* tokens, symbol* symbolTable);
