8e7798d41b9e5ceac7e13285f05b831e60fb67ad
highwiz <highwizflame875@gmail.com>
2015-11-14 21:24:40 -0500

    intial start on generator

--- src/generator.c
+++ src/generator.c
@@ -1,5 +1,35 @@
 #include "generator.h"
 
+#define MAXTABLE 100
+// the struct and this will be moved to a header at some point
+
+struct emit
+{
+	char operation;
+	
+	int lexLevel;
+
+	int addresslocation;
+	// might need to make these all arrays of sub 
+	
+};
+
 void generate(FILE* mcodePtr, char* op, int level, int address) {
+
+	int codeindex = 0;
+	struct emit emitter[MAXTABLE];
+	// this size will be defined at some point not sure yet
+
+	if(codeindex > MAXTABLE)
+		return -1;
+	else
+	{
+		emitter[codeindex].operation = op;
+		emitter[codeindex].lexLevel = level;
+		emitter[codeindex].addresslocation = address;
+
+		codeindex++;
+	}
+
 
 }
