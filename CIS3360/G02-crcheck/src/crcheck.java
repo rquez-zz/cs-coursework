0aee288386f41f723d321163678a27bfc2bc78ed
Ricardo Vasquez <rvzxjr@gmail.com>
2015-11-19 15:55:01 -0500

    Start crc program, stub methods

---
+++ CIS3360/G02-crcheck/src/crcheck.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+/**
+ * Created by Ricardo Vasquez on 11/19/2015.
+ * Program 2: CRC Codes
+ */
+public class crcheck {
+
+    static char[][] read(Reader reader) throws IOException {
+    }
+
+    static int calculate() {
+
+        return 0;
+    }
+
+    static boolean verify(int crc) {
+
+        return false;
+    }
+
+    public static void main(String args[]) {
+
+    }
+}
