4804385c33ff5cc778dbb216fc7403c2d2ef7c91
Ricardo Vasquez <rvzxjr@gmail.com>
2015-11-24 22:42:23 -0500

    Add pm0 source and header file

---
+++ src/pm0.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    int opcode;
+    int lex;
+    int param;
+} instruction;
+
+FILE* openFile(const char* path, const char* op);
+int stack(const char* inputPath, const char* outputPath);
+void read(FILE* inputPtr, instruction* instructions);
+char* buildInstructionsString(instruction* instrutions);
+char* buildTraceLine(int prevPC,
+        instruction* IR, int PC, int BP, int SP, int* stack);
+void execute(instruction* IR, int* PC, int* SP, int* BP, int* halt, int* stack);
+char* getOpcodeName(int opcode);
+char* stackString(int* stack, int SP, int BP);
