42b792ca32e6fdf98bcc6c512457bd46c1fcdeeb
Ricardo Vasquez <rvzxjr@gmail.com>
2016-01-14 19:26:40 -0500

    Added more brute force algorithms for practice

--- COP4516/Practice/HP01-brute-force/src/Permutations.java
+++ COP4516/Practice/HP01-brute-force/src/Permutations.java
@@ -65,6 +65,112 @@ public class Permutations {
         permutate(string.toCharArray(), k);
     }
 
+    /**
+     * Using a boolean array to decide if an element should be included in the subset
+     * @param p
+     * @param in
+     * @param set
+     */
+    public static void subsets(int p, boolean[] in, int[] set) {
+        if (p == set.length) {
+            for (int i = 0; i < set.length; i++) {
+                if (in[i])
+                    System.out.print(set[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        // L[p] is not in the subset
+        in[p] = false;
+        subsets(p+1, in, set);
+        // L[p] is in the subset
+        in[p] = true;
+        subsets(p+1, in, set);
+    }
+
+    /**
+     * Loops through each bit in the mask up to set.length positions
+     * @param set
+     */
+    public static void subsets(int[] set) {
+        for (int mask = 0; mask < (1 << set.length); mask++) {
+            for (int k = 0; k < set.length; k++) {
+                if ((mask & (1<<k)) > 0) {
+                    System.out.print(set[k] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * Builds combinations with increasing size up to k
+     * @param p, the index for the set
+     * @param k, the combination size
+     * @param size, the growing size of the subset up to k
+     * @param combo, the array holding the building combination
+     * @param set
+     */
+    public static void combinations(int p, int k, int size, int[] combo, int[] set) {
+
+        // This stops p from going beyond the set length, which causes stackoverflow
+        if (set.length - p < k - size) return;
+        if (size == k) {
+
+            for (int i : combo) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        // Skip set[p]
+        combinations(p+1, k, size, combo, set);
+        // Use set[p] in the combo
+        combo[size] = set[p];
+        combinations(p + 1, k, size + 1, combo, set);
+
+    }
+
+    /**
+     * Builds the combinations using a bit mask.
+     * The combination is only used when the bit mask has set.length 1 bits so
+     * I have to count the bits first and then print that combination.
+     * @param k
+     * @param set
+     */
+    public static void combinations(int k, int[] set) {
+        for (int mask = 0; mask < (1 << set.length); mask++) {
+
+            int temp = mask;
+            int count = 0;
+            while (temp != 0) {
+                count += temp & 1;
+                temp >>= 1;
+            }
+
+            if (k == count){
+                for (int i = 0; i < set.length; i++) {
+                    if ((mask & (1 << i)) > 0) {
+                        System.out.print(set[i] + " ");
+                    }
+                }
+            System.out.println();
+            }
+        }
+    }
+
+    /**
+     * Calculates the gcd of a and b using euclidean algorithm
+     * @param a
+     * @param b
+     * @return
+     */
+    public static int gcd(int a, int b) {
+        return b == a ? a : gcd(b, a % b);
+    }
+
     private static void swap(char[] array, int i, int k) {
         char temp = array[i];
         array[i] = array[k];
--- COP4516/Practice/HP01-brute-force/src/PermutationsTest.java
+++ COP4516/Practice/HP01-brute-force/src/PermutationsTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Random;
 
 import static junit.framework.TestCase.assertEquals;
 
@@ -17,17 +18,15 @@ public class PermutationsTest {
         System.setOut(new PrintStream(outContent));
     }
 
-    @After
-    public void cleanUp() {
-    }
-
     @Test
     public void testPermutateIntArray() {
+
         int length = 3;
 
         int[] array = new int[length];
         for (int i = 0; i < length; i++)
             array[i] = i + 1;
+
         Permutations.permutate(array, 0);
         String[] permutations = outContent.toString().split(" \n");
 
@@ -54,14 +53,89 @@ public class PermutationsTest {
 
         assertEquals(factorial(length), permutations.length);
 
-        System.setOut(systemOut);
-        for (String s : permutations) {
-            System.out.println(s);
+    }
+
+    @Test
+    public void testSubsetsBoolean() {
+
+        int length = 3;
+        Random rand = new Random();
+        int[] array = new int[length];
+        for (int i = 0; i < array.length; i++) {
+           array[i] = rand.nextInt(11);
         }
+
+        Permutations.subsets(0, new boolean[length], array);
+        String[] subsets = outContent.toString().split(" \n");
+        assertEquals(subsets.length, (int)Math.pow(2, length) - 1);
+    }
+
+    @Test
+    public void testSubsetsBitmasking() {
+
+        int length = 3;
+        Random rand = new Random();
+        int[] array = new int[length];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rand.nextInt(11);
+        }
+
+        Permutations.subsets(array);
+        String[] subsets = outContent.toString().split(" \n");
+        assertEquals(subsets.length, (int)Math.pow(2, length) - 1);
+        printOutSet(subsets, array);
+    }
+
+    @Test
+    public void testCombinations() {
+
+        int length = 4;
+        Random rand = new Random();
+        int[] array = new int[length];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rand.nextInt(11);
+        }
+
+        Permutations.combinations(0, 2, 0, new int[2], array);
+        String[] combos = outContent.toString().split(" \n");
+        assertEquals(combos.length, combos(4,2));
+        printOutSet(combos,array);
+    }
+
+    @Test
+    public void testCombinationsBit() {
+        int n = 4;
+        int k = 2;
+
+        Random rand = new Random();
+        int[] array = new int[n];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rand.nextInt(11);
+        }
+
+        Permutations.combinations(k,array);
+        String[] combos = outContent.toString().split(" \n");
+        assertEquals(combos.length, combos(n,k));
+        printOutSet(combos,array);
     }
 
     private int factorial(int num) {
         if (num == 1) return 1;
         return num * factorial(num - 1);
+    }
+
+    private int combos(int num, int k) {
+        return factorial(num) / (factorial(k) * factorial(num - k));
+    }
+
+    private void printOutSet(String[] set, int[] array) {
+        System.setOut(systemOut);
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (String i : set) {
+            System.out.println(i);
+        }
     }
 }
