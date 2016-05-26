f96237d2fcd7897e27a8643e10ef381d43e63026
Ricardo Vasquez <Ricardo Vasquez>
2016-05-26 14:43:54 -0400

    Add controller class that manages stations

---
+++ CNT4714/01-Multithreading/src/StationController.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+/**
+ * Name: Ricardo Vasquez
+ * Course: CNT 4714 Summer 2016
+ * Assignment title: Project 1 – Multi-threaded programming in Java
+ * Date: May 31, 2016
+ * Class: StationController
+ */
+public class StationController {
+
+    private List<Station> stations;
+
+    public StationController(List<Station> stations) {
+        this.stations = stations;
+    }
+}
