f6476bb63a6f8e9637cb9c5e809f94c01d2a2751
rquez <rvzxjr@gmail.com>
2016-05-30 19:31:41 -0400

    Implement the main method and finish the program

--- CNT4714/01-Multithreading/src/WaterTreatmentPlant.java
+++ CNT4714/01-Multithreading/src/WaterTreatmentPlant.java
@@ -1,3 +1,7 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Name: Ricardo Vasquez
  * Course: CNT 4714 Summer 2016
@@ -6,4 +10,50 @@
  * Class: WaterTreatmentPlant
  */
 public class WaterTreatmentPlant {
+
+    public static void main(String[] args) {
+
+        // Read in configuration
+        WaterTreatmentPlantConfiguration waterPlantConfiguration;
+        try {
+            waterPlantConfiguration = new WaterTreatmentPlantConfiguration();
+        } catch (IOException e) {
+            // Stop completely if we can't get a proper configuration
+            throw new RuntimeException(e.getMessage());
+        }
+
+        // Build stations and pipes
+        List<Station> stations = buildStations(waterPlantConfiguration.getNumStations());
+        List<Pipe> pipes = buildPipes(waterPlantConfiguration.getNumPipes());
+
+        // Build runnable station controllers
+        List<StationController> stationControllers = new ArrayList<>();
+        for (Station station : stations) {
+            stationControllers.add(new StationController(
+                    station,
+                    pipes,
+                    waterPlantConfiguration.getWorkLoads().get(station.getStationNumber())));
+        }
+
+        // Run all threads
+        for (StationController controller : stationControllers) {
+            (new Thread(controller)).start();
+        }
+    }
+
+    public static List<Pipe> buildPipes(int numPipes) {
+        List<Pipe> list = new ArrayList<>();
+        for (int i = 0; i < numPipes; i++) {
+            list.add(new Pipe(i));
+        }
+        return list;
+    }
+
+    public static List<Station> buildStations(int numStations) {
+        List<Station> list = new ArrayList<>();
+        for (int i = 0; i < numStations; i++) {
+            list.add(new Station(i));
+        }
+        return list;
+    }
 }
